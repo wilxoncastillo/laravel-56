50822994292b2529935285a6ab2632b99fdd862d
wilxoncastillo <wilxoncastillo@gmail>
2019-09-29 19:15:43 -0300

    lesion 3 routas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('bienvenido', function () {
+    return 'Hola Bienvenido...!';
+});
+
+Route::get('fotos/{numero?}', function($numero = 'Sin numero') {
+    return "Foto nro $numero";
+})->where('numero','[0-9]+');
+
+//Route::view('galeria', 'fotos');
+
+Route::get('galeria', function () {
+    return view('fotos', [
+        'numero' => 125
+        ]);
+});
